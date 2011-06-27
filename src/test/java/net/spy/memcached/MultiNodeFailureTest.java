a3754528f53ce411174a79f8ac542fb0d0a35350
unknown <sat@.(none)>
2011-06-27 18:15:55 +0300

    Reconfiguration test has been added

--- src/test/java/net/spy/memcached/MultiNodeFailureTest.java
+++ src/test/java/net/spy/memcached/MultiNodeFailureTest.java
@@ -2,9 +2,12 @@ package net.spy.memcached;
 
 import java.net.InetSocketAddress;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import junit.framework.TestCase;
+import net.spy.memcached.vbucket.ConfigurationProviderHTTP;
+import net.spy.memcached.vbucket.config.Bucket;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.membase.jmembase.JMembase;
@@ -17,13 +20,20 @@ import org.membase.jmembase.JMembase;
  */
 public class MultiNodeFailureTest extends TestCase {
 
-    private static final int NUM_OF_LOOPS = 1;
-    private static final int LOOP_SLEEP_TIME = 100;
     private static final String OBJ_KEY = "blah1";
     private static final String BASE_LIST_URL = "http://localhost:8091/pools";
     private static final String BUCKET_NAME = "default";
     private static final String USER_PASSWORD = "password";
     private static final String USER_NAME = "Administrator";
+    private static final List<URI> BASE_LIST = new ArrayList<URI>();
+
+    static {
+        try {
+            BASE_LIST.add(new URI(BASE_LIST_URL));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
 
     private JMembase jMembase;
     private MemcachedClient memcachedClient;
@@ -44,10 +54,7 @@ public class MultiNodeFailureTest extends TestCase {
                 logger.error(ex);
             }
 
-            List<URI> baselist = new ArrayList<URI>();
-            baselist.add(new URI(BASE_LIST_URL));
-
-            memcachedClient = new MemcachedClient(baselist, BUCKET_NAME, USER_NAME, USER_PASSWORD);
+            memcachedClient = new MemcachedClient(BASE_LIST, BUCKET_NAME, USER_NAME, USER_PASSWORD);
         } catch (Exception e) {
             logger.error(e);
             throw e;
@@ -57,24 +64,58 @@ public class MultiNodeFailureTest extends TestCase {
     public void testNodeFail() throws Exception {
         memcachedClient.set(OBJ_KEY, 100000, OBJ_KEY);
 
-        //Fail primary node
-        MemcachedNode node = memcachedClient.getNodeLocator().getPrimary(OBJ_KEY);
-        InetSocketAddress address = (InetSocketAddress) node.getSocketAddress();
-        jMembase.failNode(address.getPort());
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException ex) {
+            logger.error(ex);
+        }
+
+        failPrimaryNode();
 
         try {
-            for (int i = 0; i < NUM_OF_LOOPS; i++) {
-                memcachedClient.get(OBJ_KEY);
-                Thread.sleep(LOOP_SLEEP_TIME);
-            }
+            memcachedClient.get(OBJ_KEY);
         } catch (Exception e) {
             logger.error(e);
-            fail("Fail during setting data with 50% non active nodes");
+            fail("Fail during getting data with 50% non active nodes");
+        }
+    }
+
+    public void testReconfiguration() throws Exception {
+        memcachedClient.set(OBJ_KEY, 100000, OBJ_KEY);
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException ex) {
+            logger.error(ex);
+        }
+
+        failPrimaryNode();
+
+        //TODO client should has function to get bucket information
+        ConfigurationProviderHTTP configurationProvider = new ConfigurationProviderHTTP(BASE_LIST, USER_NAME, USER_PASSWORD);
+        Bucket bucket = configurationProvider.getBucketConfiguration(BUCKET_NAME);
+
+        memcachedClient.reconfigure(bucket);
+
+        try {
+            memcachedClient.get(OBJ_KEY);
+        } catch (Exception e) {
+            logger.error(e);
+            fail("Fail during getting after bucket reconfiguration");
         }
     }
 
     public void tearDown() throws Exception {
         jMembaseRunner.interrupt();
         jMembase.close();
+    }
+
+    /**
+     * Fail primary node
+     */
+    private void failPrimaryNode() {
+        MemcachedNode node = memcachedClient.getNodeLocator().getPrimary(OBJ_KEY);
+        InetSocketAddress address = (InetSocketAddress) node.getSocketAddress();
+        jMembase.failNode(address.getPort());
     }
 }
